56760511fd1ff2cfc1898cb09204463bd3affe11
Tedfulk <98295827+Tedfulk@users.noreply.github.com>
2023-10-25 08:09:55 -0400

    dartheme to blog page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,8 +1,11 @@
 <script>
 	import ImageGrid from '../../components/Imagegrid/ImageGrid.svelte';
+	import { darkTheme } from '../../store';
 </script>
 
-<div class="blog-container">
+<div
+	class="blog-container {$darkTheme ? 'light-background light-text' : 'dark-background dark-text'}"
+>
 	<h1 class="title" style="padding-bottom: 2rem;">Bot Your Back Blog</h1>
 	<ImageGrid />
 </div>
@@ -10,8 +13,23 @@
 <style>
 	.blog-container {
 		padding-top: 3rem;
+		padding-bottom: 3rem;
 	}
 	.title {
 		text-align: center;
+	}
+	.dark-background {
+		background-color: #000;
+	}
+
+	.light-background {
+		background-color: whitesmoke;
+	}
+	.dark-text {
+		color: #000;
+	}
+
+	.light-text {
+		color: whitesmoke;
 	}
 </style>
